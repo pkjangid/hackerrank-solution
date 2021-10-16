4b1c1da1cb1e8bc9500e40520ecf55fe4da83ca4
Pritam Jangid <75982077+pkjangid@users.noreply.github.com>
2021-10-16 16:19:55 +0530

    Create Mini-Max Sum.c

---
+++ Problem Solving/Mini-Max Sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    long long int arr[5],max=0,min,sum=0;
+    for(int i=0;i<5;i++){
+        scanf("%lld",&arr[i]);
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        sum+=arr[i];
+    }
+    min =arr[0];
+    for(int i=0;i<5;i++)
+        {
+        if(arr[i]<min)
+            min=arr[i];
+    }
+    printf("%lld %lld",sum-max,sum-min);
+}
